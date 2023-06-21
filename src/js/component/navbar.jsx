19b652c72b4368702aaa33da102259ecfbd4b392
gaha20 <128089822+gaha20@users.noreply.github.com>
2023-06-21 23:44:05 +0000

    Navbar styles

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -2,25 +2,25 @@ import React from "react";
 
 const Navbar = () => {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg navbar-light bg-dark">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
+                <a class="navbar-brand text-white" href="#">Start Bootstrap</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
+                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link  text-white active" aria-current="page" href="#">Home</a>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link" href="#">Features</a>
+                    <a class="nav-link text-white" href="#">About</a>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
+                    <a class="nav-link text-white" href="#">Service</a>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link disabled">Disabled</a>
+                    <a class="nav-link text-white">Contact</a>
                     </li>
                 </ul>
                 </div>
