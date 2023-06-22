577015c9fd710c65aacbdef4865b2db9dc689f84
gaha20 <128089822+gaha20@users.noreply.github.com>
2023-06-22 17:50:17 +0000

    cards formating

--- src/js/component/cards.jsx
+++ src/js/component/cards.jsx
@@ -5,12 +5,12 @@ const Cards = () => {
 
     <div className="p-2">
         <div className="card" style={{width: "18rem"}}>
-            <img src="https://picsum.photos/id/870/200/300?grayscale&blur=2" className="card-img-top" alt="..."/>
-            <div className="card-body">
+            <img src="https://picsum.photos/id/870/500/325?grayscale&blur=2" className="card-img-top" alt="..."/>
+            <div className="card-body text-center">
                 <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus soluta sapiente perspiciatis sed qui consequuntur, est ut minus odio nisi a nulla at, quas repellendus aliquam itaque culpa amet! Tempora?</p>
                 <div className="card-footer">
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                    <a href="#" className="btn btn-primary">Find Out More!</a>
                 </div>
             </div>
         </div>    
